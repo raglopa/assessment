5d2c9c169b0f21cc918708fa99b9c916cb02c2d5
Attila Polgar <raglopa@gmail.com>
2018-07-03 12:21:10 +0200

    test: cover IssueFilter reducer

--- ios-github/src/components/IssueFilter/test.js
+++ ios-github/src/components/IssueFilter/test.js
@@ -3,6 +3,10 @@ import { noErrorsAllowed } from '../../common/test-utils'
 import IssueFilter from './'
 import renderer from 'react-test-renderer'
 
+import reducer from './reducer'
+import { issueStatusFilterChanged } from './actions'
+import { ISSUE_STATUS_FILTER_CHANGED } from './action-types'
+
 noErrorsAllowed()
 
 describe('IssueFilter component', () => {
@@ -10,5 +14,20 @@ describe('IssueFilter component', () => {
     //TODO: test connected components
     const rendered = renderer.create(<IssueFilter />).toJSON()
     expect(rendered).toBeTruthy()
+  })
+})
+
+describe("IssueFilter's reducer", () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({ statusFilter: 'open' })
+  })
+
+  it('should handle ISSUE_STATUS_FILTER_CHANGED', () => {
+    const openState = { statusFilter: 'open' }
+    const closedState = { statusFilter: 'closed' }
+    const action = issueStatusFilterChanged()
+
+    expect(reducer(openState, action)).toEqual(closedState)
+    expect(reducer(closedState, action)).toEqual(openState)
   })
 })
